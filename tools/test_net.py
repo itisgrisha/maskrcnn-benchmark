b46b94370ebaad1c6f76b9aa787d670276904c27
itisgrisha <itisgrisha@gmail.com>
2019-06-30 17:58:20 +0300

    not distributed

--- tools/test_net.py
+++ tools/test_net.py
@@ -36,7 +36,7 @@ def eval_net(model):
             output_folder = os.path.join(cfg.OUTPUT_DIR, "inference", dataset_name)
             mkdir(output_folder)
             output_folders[idx] = output_folder
-    data_loaders_val = make_data_loader(cfg, is_train=False, is_distributed=distributed)
+    data_loaders_val = make_data_loader(cfg, is_train=False, is_distributed=False)
     for output_folder, dataset_name, data_loader_val in zip(output_folders, dataset_names, data_loaders_val):
         inference(
             model,
