4e9126f3560c6333a2f16e43b3e648013a3baafa
itisgrisha <itisgrisha@gmail.com>
2019-07-02 10:58:35 +0300

    rm amp focal

--- maskrcnn_benchmark/layers/nms.py
+++ maskrcnn_benchmark/layers/nms.py
@@ -4,9 +4,11 @@ from maskrcnn_benchmark import _C
 
 from apex import amp
 
+amp.register_float_function(_C, 'nms')
+
+nms = _C.nms
 # Only valid with fp32 inputs - give AMP the hint
 #nms = amp.float_function(_C.nms)
-nms = amp.promote_function(_C.nms)
-#amp.register_float_function(_C, 'nms')
+#nms = amp.promote_function(_C.nms)
 # nms.__doc__ = """
 # This function performs Non-maximum suppresion"""
