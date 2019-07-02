66b18a89478e2ba5ebbf873845479e76cbc79aa8
itisgrisha <itisgrisha@gmail.com>
2019-07-02 13:50:59 +0300

    profile

--- maskrcnn_benchmark/modeling/rpn/retinanet/inference.py
+++ maskrcnn_benchmark/modeling/rpn/retinanet/inference.py
@@ -69,6 +69,7 @@ class RetinaNetPostProcessor(RPNPostProcessor):
         A = box_regression.size(1) // 4
         C = box_cls.size(1) // A
         print(anchors)
+        print(anchors[0].bbox)
         # put in the same format as anchors
         box_cls = permute_and_flatten(box_cls, N, A, C, H, W)
         box_cls = box_cls.sigmoid()
